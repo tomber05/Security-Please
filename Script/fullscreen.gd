d3aa0481276010a0bfd76185ece2776e9a427c42
marta-gonz <darkhorsevoraz@gmail.com>
2026-02-24 08:59:56 +0100

    menufin

---
+++ Script/fullscreen.gd
@@ -0,0 +1,18 @@
+extends CheckButton
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass
+
+
+func _on_toggled(toggled_on: bool) -> void:
+	if toggled_on == true:
+		DisplayServer.window_set_mode(DisplayServer.WINDOW_MODE_FULLSCREEN)
+	else:
+		DisplayServer.window_set_mode(DisplayServer.WINDOW_MODE_WINDOWED)
